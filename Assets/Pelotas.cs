acdec4493a6eeb2f2300a4d8836c2bb489c3f428
sebastian <sebastiansaavedra4515@gmail.com>
2022-05-25 00:10:42 -0500

    Initial commit

---
+++ Assets/Pelotas.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelotas : MonoBehaviour {
+
+	public Rigidbody rig;
+	public float force, upForce=10;
+	public GameObject Player;
+	public GameObject Spawner;
+	public bool have_force = false;
+	PhysicMaterial fisica;
+	public float rebote;
+	// Use this for initialization
+	void Start () 
+	{
+		this.fisica = new PhysicMaterial();
+		this.fisica.bounciness = this.rebote;
+		this.fisica.frictionCombine = PhysicMaterialCombine.Minimum;
+		this.fisica.bounceCombine = PhysicMaterialCombine.Maximum;
+		force = Random.Range(3, upForce);
+		GetComponent<SphereCollider>().material = this.fisica;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 direction = Player.transform.position - this.Spawner.transform.position;
+		Move(direction);
+    }
+	public void Move(Vector3 dir)
+    {
+		if(!have_force)
+        {
+			rig.AddForce(dir.normalized * this.force, ForceMode.Impulse);
+			rig.AddForce(this.transform.up * this.force, ForceMode.Impulse);
+        }
+		this.have_force = true;
+    }
+	void OnCollisionEnter(Collision collision)
+    {
+		//if(collision.gameObject.tag == "Player")
+  //      {
+		//	Destroy(this.gameObject);
+		//}
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour 
+{
+	public GameObject pelota;
+	public GameObject Player;
+	public Transform Posicionarrojo;
+	public float TimeMax=4;
+	public float time=0;
+	public Animator anim;
+	// Use this for initialization
+	void Start()
+	{
+		
+		this.pelota.GetComponent<Pelotas>().Player = this.Player;
+		this.pelota.GetComponent<Pelotas>().Spawner = this.gameObject;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		time += Time.deltaTime;
+
+		if (time>=TimeMax)
+		{
+			anim.SetTrigger("Lanzar");
+			if(time>=TimeMax+1)
+			{
+				pelota.transform.position = new Vector3(this.Posicionarrojo.position.x, this.Posicionarrojo.position.y, this.Posicionarrojo.position.z);
+				GameObject pelotaLanzada = Instantiate(pelota);
+				TimeMax = Random.Range(2, 4);
+				time = 0;
+
+			}
+		}
+	}
+}
